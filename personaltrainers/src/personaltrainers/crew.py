1ceb9ec4d49a20cdcd155b3d75d35160a117d256
EmanueleCeglia <emanueleceglia0@gmail.com>
2025-05-19 00:08:55 +0200

    refined tasks

--- personaltrainers/src/personaltrainers/crew.py
+++ personaltrainers/src/personaltrainers/crew.py
@@ -2,7 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent, BaseModel
 from crewai.tools import tool
-from typing import List
+from typing import List, Dict
 from tools.db_call import db_call
 import pandas as pd
 
@@ -10,7 +10,10 @@ class WhereClause(BaseModel):
     clause: str
 
 class MacroExercises(BaseModel):
-    clause: dict
+    exercises: str
+
+class FinalExercises(BaseModel):
+    exercises: List[Dict]
 
 @tool("sql_query_tool")
 def sql_query_tool(where_clause: str) -> dict:
@@ -34,7 +37,7 @@ class PersonalTrainers():
     def sql_query_generator(self) -> Agent:
         return Agent(
             config=self.agents_config['sql_query_generator'], 
-            verbose=False
+            verbose=True
         )
     
     @agent
@@ -42,14 +45,14 @@ class PersonalTrainers():
         return Agent(
             config=self.agents_config['sql_query_caller'], 
             tools=[sql_query_tool],
-            verbose=False
+            verbose=True
         )
     
     @agent
     def esercises_selector(self) -> Agent:
         return Agent(
             config=self.agents_config['esercises_selector'], 
-            verbose=False
+            verbose=True
         )
 
 
@@ -71,7 +74,8 @@ class PersonalTrainers():
             expected_output="A dictionary with the selected exercises.",
             agent=self.sql_query_caller(),
             tools=[sql_query_tool],
-            context=[self.fetch_exercises()]
+            context=[self.fetch_exercises()],
+            output_pydantic=MacroExercises
         )
 
 
@@ -79,7 +83,8 @@ class PersonalTrainers():
     def select_exercises(self) -> Task:
         return Task(
             config=self.tasks_config['select_exercises'],
-            context=[self.run_query()]
+            context=[self.run_query()],
+            output_pydantic=FinalExercises
         )
 
 
@@ -91,6 +96,6 @@ class PersonalTrainers():
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
-            verbose=False,
+            verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
